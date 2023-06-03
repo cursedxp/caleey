511b98b126f3dbd943de44397521eb249f264a06
Anil Ozsoy <anilozsoy@gmail.com>
2023-06-03 12:46:25 +0300

    logo has been added to top nav

--- src/app/components/TopNavigation/TopNavigation.tsx
+++ src/app/components/TopNavigation/TopNavigation.tsx
@@ -1,10 +1,16 @@
 import "./TopNavigation.scss";
+import Logo from "../../../../public/assets/img/caleeyLogo.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function TopNavigation() {
   return (
-    <header>
-      <div className="company h-16 w-full shadow-sm"></div>
-      <div className="menubar"></div>
+    <header className="flex h-16 w-full shadow-sm p-4">
+      <Link href="" className=" flex items-center font-medium text-lg ml-6">
+        <Image src={Logo} alt="Company Logo" className=" mr-4"></Image>
+        Calleey
+      </Link>
+      <ul className="menubar"></ul>
     </header>
   );
 }
