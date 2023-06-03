1e0c686902da4f2e3ecf453d26ea09f81e8eae90
Anil Ozsoy <anilozsoy@gmail.com>
2023-06-03 12:25:52 +0300

    top navigation comp has been added to project

---
+++ src/app/components/TopNavigation/TopNavigation.tsx
@@ -0,0 +1,10 @@
+import "./TopNavigation.scss";
+
+export default function TopNavigation() {
+  return (
+    <header>
+      <div className="company h-16 w-full shadow-sm"></div>
+      <div className="menubar"></div>
+    </header>
+  );
+}
