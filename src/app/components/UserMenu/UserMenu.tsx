dde7c647f41d91051764fca99c821288f9376d83
Anil Ozsoy <anilozsoy@gmail.com>
2023-06-04 12:27:51 +0300

    adding menu close functionality

--- src/app/components/UserMenu/UserMenu.tsx
+++ src/app/components/UserMenu/UserMenu.tsx
@@ -4,11 +4,10 @@ import {
   CreditCardIcon,
   UserIcon,
   ArrowLeftOnRectangleIcon,
-  SunIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import "./UserMenu.scss";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 export default function UserMenu() {
@@ -18,9 +17,27 @@ export default function UserMenu() {
     setShowMenu(!showMenu);
   };
 
+  const handleOutsideClick = useRef((event: any) => {
+    //checks if the element that was clicked on, or any of its ancestors in the DOM, has a class of .menu-button
+    if (showMenu && !event.target.closest(".menu-button")) {
+      setShowMenu(false);
+    }
+  });
+
+  useEffect(() => {
+    const handleClick = handleOutsideClick.current;
+    //listens the event and calls handleOutsideClick if the event is happen
+    window.addEventListener("click", handleClick);
+
+    //"cleanup function". In this case, the cleanup function is being used to remove an event listener from the window object.
+    return () => {
+      window.removeEventListener("click", handleClick);
+    };
+  }, [showMenu]);
+
   return (
     <div
-      className=" p-2 rounded-lg hover:bg-gray-200 relative hover:cursor-pointer"
+      className="menu-button p-2 rounded-lg hover:bg-gray-200 relative hover:cursor-pointer"
       onClick={handleClick}
     >
       <EllipsisVerticalIcon className="h-6 w-6" />
