ce12f91e3cf584c263b349d951a2736f1e7af750
Anil Ozsoy <anilozsoy@gmail.com>
2023-06-20 16:34:35 +0200

    searchbar styling update

--- src/app/components/Searchbar/SearchBar.tsx
+++ src/app/components/Searchbar/SearchBar.tsx
@@ -5,7 +5,7 @@ export default function SearchBar() {
   const [searchText, setSearchText] = useState();
 
   return (
-    <div className="search shadow-md rounded-lg w-80 focus:outline-none focus:ring-2 focus:ring-blue-500">
+    <div className="search shadow rounded-lg w-80 focus:outline-none focus:ring-2 focus:ring-blue-500">
       <label className="flex items-center">
         <MagnifyingGlassIcon className="w-6 h-6 inline-block mx-2 text-slate-700" />
         <input
